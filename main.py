534620e521f659de562e01cfd845617d2a05b564
AkshitaSharma211 <sharma.1608akshita@gmail.com>
2026-07-08 17:03:59 +0530

    Add Pydantic model, POST and GET all routes

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Student(BaseModel):
+    name: str
+    age: int
 
 app = FastAPI()
+
+students_db = []
 
 @app.get("/")
 def read_root():
     return {"message": "hello"}
+
+
+@app.get("/students/all")
+def list_all_students():
+    return students_db
 
 @app.get("/students/{student_id}")
 def get_student(student_id: int):
@@ -18,3 +30,8 @@ def list_students(skip: int = 0, limit: int = 10):
 @app.get("/students/{student_id}/grades")
 def get_student_grades(student_id: int, subject: str = "all"):
     return {"student_id": student_id, "subject": subject}
+
+@app.post("/students")
+def create_student(student: Student):
+    students_db.append(student)
+    return student
